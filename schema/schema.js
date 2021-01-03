137a5cc498748320fd0ce44adfdd0e5b6bad0a1c
rene <renegmed@yahoo.com>
2021-01-03 11:47:47 -0500

    section 3: root queries

--- schema/schema.js
+++ schema/schema.js
@@ -13,3 +13,13 @@ const UserType = new GraphQLObjectType({
         age: { type: GraphQLInt } 
     }
 });
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString } }
+        }
+    }
+});
